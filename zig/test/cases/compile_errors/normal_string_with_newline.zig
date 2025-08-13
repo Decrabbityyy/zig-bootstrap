f80a0828ffd43d1e773e42740a40d6b8643d7d99
Decrabbit <decrabbit@163.com>
2025-08-13 22:00:13 +0800

    set zig as submodule

--- zig/test/cases/compile_errors/normal_string_with_newline.zig
+++
@@ -1,8 +0,0 @@
-const foo = "a
-b";
-
-// error
-// backend=stage2
-// target=native
-//
-// :1:15: error: string literal contains invalid byte: '\n'
--- zig/test/cases/compile_errors/ptrcast_to_non-pointer.zig
+++
@@ -1,9 +0,0 @@
-export fn entry(a: *i32) usize {
-    return @ptrCast(a);
-}
-
-// error
-// backend=stage2
-// target=native
-//
-// :2:12: error: expected pointer type, found 'usize'
